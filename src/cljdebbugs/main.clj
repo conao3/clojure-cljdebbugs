d5f968f23d74506ac31bd85b06b3c2d2a02da1d5
Naoya Yamashita <conao3@gmail.com>
2024-06-30 12:58:00 +0900

    fetch soap server

--- src/cljdebbugs/main.clj
+++ src/cljdebbugs/main.clj
@@ -1,6 +1,7 @@
 (ns cljdebbugs.main
   (:require
-   [clojure.data.xml :as xml])
+   [clojure.data.xml :as xml]
+   [babashka.http-client :as http])
   (:gen-class))
 
 (xml/alias-uri 'soap "http://schemas.xmlsoap.org/soap/envelope/")
@@ -13,16 +14,20 @@
   (xml/indent-str (xml/sexp-as-element sexp)))
 
 (defn -main [& _args]
-  (render-xml
-   [::soap/Envelope
-    {::soapenc/encodingStyle "https://schemas.xmlsoap.org/soap/encoding/"
-     :xmlns/soapenc "http://schemas.xmlsoap.org/soap/encoding/"
-     :xmlns/xsd "http://www.w3.org/2001/XMLSchema"
-     :xmlns/xsi "http://www.w3.org/2001/XMLSchema-instance"}
-    [::soap/Body
-     [::t/get_staus
-      [::t/bugs {::xsi/type "soapenc:Array"
-                 ::soapenc/arrayType "xsd:int[3]"}
-       [::t/bugs {::xsi/type "xsd:int"} 753]
-       [::t/bugs {::xsi/type "xsd:int"} 837]
-       [::t/bugs {::xsi/type "xsd:int"} 841]]]]]))
+  (let [body (render-xml
+              [::soap/Envelope
+               {::soapenc/encodingStyle "https://schemas.xmlsoap.org/soap/encoding/"
+                :xmlns/soapenc "http://schemas.xmlsoap.org/soap/encoding/"
+                :xmlns/xsd "http://www.w3.org/2001/XMLSchema"
+                :xmlns/xsi "http://www.w3.org/2001/XMLSchema-instance"}
+               [::soap/Body
+                [::t/get_status
+                 [::t/bugs {::xsi/type "soapenc:Array"
+                            ::soapenc/arrayType "xsd:int[3]"}
+                  [::t/bugs {::xsi/type "xsd:int"} 753]
+                  [::t/bugs {::xsi/type "xsd:int"} 837]
+                  [::t/bugs {::xsi/type "xsd:int"} 841]]]]])
+        res (http/post "https://debbugs.gnu.org/cgi/soap.cgi?WSDL"
+                       {:headers {:content-type "text/xml"}
+                        :body body})]
+    (println (:body res))))
